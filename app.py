3907433b4d09a03a4b4a885a03ba3fd84df663c8
afzaal <m.afzaal@sparkai.ca>
2024-03-14 21:26:29 +0500

    Stripe Payment

--- app.py
+++ app.py
@@ -1,62 +1,72 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-import sqlite3
+import stripe
+
 
 app = Flask(__name__)
 CORS(app, resources={r"*": {"origins": "*"}})
-DATABASE = 'products.db'
+stripe.api_key = "sk_test_51OqjevGueFI8a5iZU7p2CPvQcQ89PjXNPZ4UHzJN559DrDaZlSnsfraCymuRdNskAyUnCXRWSzTPT22yZIdsMLFl00RYM2MJcv"
 
 
-def create_db():
-    conn = sqlite3.connect(DATABASE)
-    cursor = conn.cursor()
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS products (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT NOT NULL,
-            price REAL NOT NULL,
-            img_url TEXT NOT NULL,
-            vendor TEXT NOT NULL
-        )
-    ''')
-    conn.commit()
-    conn.close()
+@app.route('/api/payment', methods=['POST'])
+def create_stripe_user():
+    """
+    Creates a new Stripe user with the given payment method and email.
 
+    Parameters:
+        p_method (str): The payment method to associate with the new user.
+        email (str): The email address of the new user.
 
-create_db()  # Initialize the database when the app starts
-
-
-@app.route('/api/products', methods=['GET'])
-def get_products():
-    conn = sqlite3.connect(DATABASE)
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM products')
-    products = cursor.fetchall()
-    conn.close()
-
-    product_list = []
-    for product in products:
-        product_dict = {'id': product[0], 'name': product[1], 'price': product[2], 'img_url': product[3], 'vendor': product[4]}
-        product_list.append(product_dict)
-
-    return jsonify({'products': product_list})
-
-
-@app.route('/api/products', methods=['POST'])
-def add_product():
+    Returns:
+        tuple: A tuple containing a string indicating the success status ('success' if successful, None otherwise) and the created customer object if successful, or a tuple containing None and the error message if unsuccessful.
+    """
     data = request.get_json()
-    name = data['name']
-    price = data['price']
-    img_url = data['img_url']
-    vendor = data['vendor']
+    p_method = data['payment_method_id']
+    amount = data['amount']
+    try:
+        customer = stripe.Customer.create(
+            payment_method=p_method
+        )
 
-    conn = sqlite3.connect(DATABASE)
-    cursor = conn.cursor()
-    cursor.execute('INSERT INTO products (name, price, img_url, vendor ) VALUES (?, ?, ?, ?)', (name, price, img_url, vendor))
-    conn.commit()
-    conn.close()
+        if customer:
+            if create_stripe_intent(customer, p_method, amount):
+                return jsonify({'message': 'Payment successful'}), 200
+        return jsonify({'message': 'Payment failed'}), 400
+    except Exception as e:
+        return jsonify({'message': 'Payment failed'}), 400
 
-    return jsonify({'message': 'Product added successfully'})
+
+def create_stripe_intent(customer, payment_method, amount):
+    """
+    Create a Stripe payment intent.
+
+    Args:
+        customer (str): The ID of the customer to associate the payment with.
+        payment_method (str): The ID of the payment method to use.
+        amount (float): The amount to charge, in USD.
+
+    Returns:
+        tuple: A tuple containing a string representing the status of the payment ('success' or None) and a PaymentIntent object.
+
+    Raises:
+        Exception: If there is an error creating the payment intent.
+
+    Example:
+        create_stripe_intent('customer_id', 'payment_method_id', 10.0)
+    """
+    try:
+        intent = stripe.PaymentIntent.create(
+            customer=customer,
+            payment_method=payment_method,
+            currency='usd',  # you can provide any currency you want
+            amount=int(float(amount) * 100),  # I modified the amount to distinguish payments
+            off_session=True,
+            confirm=True
+        )
+
+        return intent
+    except Exception as e:
+        return None, str(e)
 
 
 if __name__ == '__main__':
